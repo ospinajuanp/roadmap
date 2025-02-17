275921853b748f0b29d0023137df5d3c2af1bc1f
Ospinajuanp <incuantaqdev@gmail.com>
2025-02-17 14:25:13 -0500

    adding single-page

---
+++ single-page/node_modules/ospinajuanp-reset-css/src/index.js
@@ -0,0 +1,7 @@
+function info(){
+    console.log('In this project we are working on resetting the default styles that the browser has, in order to start the project in the best possible way without having to fix possible bugs by default styles added by browsers.')
+}
+
+module.exports={
+    info
+}
